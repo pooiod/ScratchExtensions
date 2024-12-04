94c1f47e60d67075fbbcb1b2b9582cd45bfaf712
pooiod7 <82846639+pooiod@users.noreply.github.com>
2024-12-03 20:50:35 -0500

    g

--- extras/js/pmgallary.js
+++ extras/js/pmgallary.js
@@ -14,7 +14,7 @@
 
     function checkAndInsertExtensionGallery() {
         const targetSpan = Array.from(document.querySelectorAll('span'))
-            .find(span => span.textContent.trim() === 'PenguinMod Extra Extensions');
+        .find(span => span.textContent.trim() === 'PenguinMod Extra Extensions');
 
         if (targetSpan && !document.querySelector('#p7extensionslist')) {
             const parentElement = targetSpan.closest('span').parentElement.parentElement;
@@ -22,7 +22,7 @@
             const newHTML = `
 <div id="p7extensionslist" class="library-item_library-item_1DcMO library-item_featured-item_3V2-t library-item_library-item-extension_3xus9" onclick="window.open('https://p7scratchextensions.pages.dev')">
   <div class="library-item_featured-image-container_1KIHG">
-    <img class="library-item_featured-image_2gwZ6" loading="lazy" draggable="false" src="https://p7scratchextensions.pages.dev/extras/images/P7ExtGallaryCover.png">
+    <img class="library-item_featured-image_2gwZ6" loading="lazy" draggable="false" src="https://p7scratchextensions.pages.dev/extras/images/P7ExtGalleryCover.png">
   </div>
   <div class="library-item_featured-extension-text_22A1k library-item_featured-text_2KFel">
     <span class="library-item_library-item-name_2qMXu"><span>Pooiod7's extensions</span></span><br>
