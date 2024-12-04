db20ef922496b73a3eac5bca53cc76229a662d28
pooiod7 <82846639+pooiod@users.noreply.github.com>
2024-12-04 11:27:31 -0500

    p

---
+++ ext/ScrollUtilities/dev.js
@@ -0,0 +1,70 @@
+// Work in progress
+
+(function (Scratch) {
+    'use strict';
+  
+    if (!Scratch.extensions.unsandboxed) {
+      throw new Error('This extension must run unsandboxed');
+    }
+  
+    class ScrollExtension {
+      constructor(runtime) {
+        this.isScrolling = false;
+        this.yvel = 0;
+        this.xvel = 0;
+  
+        document.addEventListener('wheel', this.handleScroll.bind(this), true);
+      }
+  
+      getInfo() {
+        return {
+          id: 'p7ScrollUtilities',
+          name: 'Scroll Detector',
+          color1: '#4CAF50',
+          color2: '#45A164',
+          blocks: [
+            {
+              opcode: 'getScrollYVelocity',
+              blockType: Scratch.BlockType.REPORTER,
+              text: 'Scroll Y Velocity',
+            },
+            {
+              opcode: 'getScrollXVelocity',
+              blockType: Scratch.BlockType.REPORTER,
+              text: 'Scroll X Velocity',
+            },
+            {
+              opcode: 'scrolldetect',
+              blockType: Scratch.BlockType.BOOLEAN,
+              text: 'Scrolling?',
+            },
+          ],
+        };
+      }
+  
+      getScrollYVelocity() {
+        const vel = this.yvel;
+        this.yvel /= 5;
+        return vel;
+      }
+      
+      getScrollXVelocity() {
+        const vel = this.xvel;
+        this.xvel /= 5;
+        return vel;
+      }
+  
+      scrolldetect() {
+        return (Math.round(this.yvel) !== 0) || (Math.round(this.xvel) !== 0);
+      }
+  
+      handleScroll(scrolldata) {
+        this.yvel = parseInt(scrolldata.deltaY);
+        this.xvel = parseInt(scrolldata.deltaX);
+        this.isScrolling = true;
+      }
+    }
+  
+    Scratch.extensions.register(new ScrollExtension());
+  })(Scratch);
+  
