1d13f76b5c536204b9e46da51c59e6afbff5099c
pooiod7 <82846639+pooiod@users.noreply.github.com>
2024-12-10 09:09:34 -0500

    j

--- extras/js/extenable.js
+++ extras/js/extenable.js
@@ -1,4 +1,31 @@
+
 if (window.location.hostname === 'studio.penguinmod.com') {
+
+    function checkAndInsertExtensionGallery() {
+        const targetSpan = Array.from(document.querySelectorAll('span'))
+        .find(span => span.textContent.trim() === 'PenguinMod Extra Extensions');
+
+        if (targetSpan && !document.querySelector('#p7extensionslist')) {
+            const parentElement = targetSpan.closest('span').parentElement.parentElement;
+
+            const newHTML = `
+<div id="p7extensionslist" class="library-item_library-item_1DcMO library-item_featured-item_3V2-t library-item_library-item-extension_3xus9" onclick="window.open('https://p7scratchextensions.pages.dev')">
+  <div class="library-item_featured-image-container_1KIHG">
+    <img class="library-item_featured-image_2gwZ6" loading="lazy" draggable="false" src="https://p7scratchextensions.pages.dev/extras/images/P7ExtGalleryCover.png">
+  </div>
+  <div class="library-item_featured-extension-text_22A1k library-item_featured-text_2KFel">
+    <span class="library-item_library-item-name_2qMXu"><span>Pooiod7's extensions</span></span><br>
+    <span class="library-item_featured-description_MjIJw"><span>Explore a large collection of Scratch extensions made by Pooiod7.</span></span>
+  </div>
+</div>
+`;
+
+            parentElement.insertAdjacentHTML('afterend', newHTML);
+        }
+    }
+
+    setInterval(checkAndInsertExtensionGallery, 1000);
+
     let checks = 0;
     let clicks = 0;
 
