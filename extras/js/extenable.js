10bb6cb0b79f0e26b4d590687e45e82a5ec528d7
pooiod7 <82846639+pooiod@users.noreply.github.com>
2024-12-13 08:36:09 -0500

    Update extenable.js

--- extras/js/extenable.js
+++ extras/js/extenable.js
@@ -9,9 +9,9 @@ if (window.location.hostname === 'studio.penguinmod.com') {
             const parentElement = targetSpan.closest('span').parentElement.parentElement;
 
             const newHTML = `
-<div id="p7extensionslist" class="library-item_library-item_1DcMO library-item_featured-item_3V2-t library-item_library-item-extension_3xus9" onclick="window.open('https://p7scratchextensions.pages.dev')">
+<div id="p7extensionslist" class="library-item_library-item_1DcMO library-item_featured-item_3V2-t library-item_library-item-extension_3xus9" onclick="window.open('https://p7extensions.pages.dev/')">
   <div class="library-item_featured-image-container_1KIHG">
-    <img class="library-item_featured-image_2gwZ6" loading="lazy" draggable="false" src="https://p7scratchextensions.pages.dev/extras/images/P7ExtGalleryCover.png">
+    <img class="library-item_featured-image_2gwZ6" loading="lazy" draggable="false" src="https://p7extensions.pages.dev/extras/images/P7ExtGalleryCover.png">
   </div>
   <div class="library-item_featured-extension-text_22A1k library-item_featured-text_2KFel">
     <span class="library-item_library-item-name_2qMXu"><span>Pooiod7's extensions</span></span><br>
@@ -36,7 +36,7 @@ if (window.location.hostname === 'studio.penguinmod.com') {
         }
 
         let targetElement = document.querySelector('p.url_url_3Y61f');
-        if (targetElement && targetElement.textContent.includes('https://p7scratchextensions.pages.dev/')) {
+        if (targetElement && (targetElement.textContent.includes('https://p7scratchextensions.pages.dev/') || targetElement.textContent.includes('https://p7extensions.pages.dev/'))) {
             let button = document.querySelector('button.security-manager-modal_allow-button_3tcXk');
             if (button) {
                 if (button.disabled) button.disabled = false;
