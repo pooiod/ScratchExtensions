12052585608978d3fd0506dc852f27a4fbd9be12
pooiod7 <portsmouthuser35@gmail.com>
2025-02-03 11:15:48 -0500

    x

--- ext/Splats/dev.js
+++ ext/Splats/dev.js
@@ -7,6 +7,41 @@
 		throw new Error('This extension must run unsandboxed');
 	}
 
+    if (!document.getElementById("SplatImportMap")) {
+        const importmap = document.createElement('script');
+        importmap.type = 'importmap';
+        importmap.id = "SplatImportMap";
+        importmap.textContent = JSON.stringify({
+            imports: {
+                "three": "https://unpkg.com/three@0.157.0/build/three.module.js",
+                "three/addons/": "https://unpkg.com/three@0.157.0/examples/jsm/",
+                "@lumaai/luma-web": "https://unpkg.com/@lumaai/luma-web@0.2.0/dist/library/luma-web.module.js"
+            }
+        });
+        document.currentScript.after(importmap);
+
+        let SplatWindowImports = document.createElement("script");
+        SplatWindowImports.type = "module";
+        SplatWindowImports.id = "SplatWindowImports";
+        SplatWindowImports.innerHTML = `
+    import { WebGLRenderer, PerspectiveCamera, Scene, Color, FogExp2 } from 'three';
+    import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+    import { LumaSplatsSemantics, LumaSplatsThree } from "@lumaai/luma-web";
+    
+    window.WebGLRenderer = WebGLRenderer;
+    window.PerspectiveCamera = PerspectiveCamera;
+    window.Scene = Scene;
+    window.Color = Color;
+    window.FogExp2 = FogExp2;
+    
+    window.OrbitControls = OrbitControls;
+    
+    window.LumaSplatsSemantics = LumaSplatsSemantics;
+    window.LumaSplatsThree = LumaSplatsThree;
+`;
+        document.head.appendChild(SplatWindowImports);
+    }
+
 	class P7Splats {
 		constructor() {
 			this.stagewidth = Scratch.vm.runtime.stageWidth;
@@ -36,9 +71,9 @@
 					{
 						opcode: 'func',
 						blockType: Scratch.BlockType.COMMAND,
-						text: 'a block [VAL]',
+						text: 'Create splat: Model [MODEL], ID [ID], Width [WIDTH], Height [HEIGHT]',
 						arguments: {
-							VAL: {
+							CONFIG: {
 								type: Scratch.ArgumentType.STRING,
 								defaultValue: 'REEEEEEEEEE',
 							},
