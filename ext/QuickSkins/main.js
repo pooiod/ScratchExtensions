d8ad43e41bcc4fde0f815fa9efd121e81af2f093
pooiod7 <portsmouthuser35@gmail.com>
2025-02-04 22:38:12 -0500

    added QuickSkins extension and also started extension archives

--- ext/QuickSkins/main.js
+++ ext/QuickSkins/main.js
@@ -1,21 +1,26 @@
-// Fast bitmap render function
+// A simple extension to render images from data uris super quickly
+
+// Feel free to add these functions to your extension
+// This is meant to be a base extension to pull code from anyways
 
 (function (Scratch) {
-    class BitmapSkinExtension {
-        constructor() {
-            this.skins = {};
-        }
+	if (!Scratch.extensions.unsandboxed) {
+		throw new Error('This extension must run unsandboxed');
+	}
+
+    class QuickSkins {
+        constructor() {}
     
         getInfo() {
             return {
-            id: "bitmapSkin",
-            name: "Bitmap Skin",
-            color1: "#4C97FF",
+                id: "P7QuickSkins",
+                name: "Quick Skins",
+                color1: "#9966ff",
                 blocks: [
                     {
-                        opcode: "setImage",
+                        opcode: "setSkin",
                         blockType: Scratch.BlockType.COMMAND,
-                        text: "Set sprite image to [DATAURI]",
+                        text: "Set sprite skin to [DATAURI]",
                         arguments: {
                             DATAURI: {
                                 type: Scratch.ArgumentType.STRING,
@@ -24,18 +29,19 @@
                         },
                     },
                     {
-                        opcode: "resetSprite",
+                        opcode: "removeSkin",
                         blockType: Scratch.BlockType.COMMAND,
-                        text: "Reset sprite to normal costume",
+                        text: "Remove sprite skin",
                     },
                 ],
             };
         }
 
-        setImage(args, util) {
+        setSkin({ DATAURI }, util) {
             const target = util.target;
             const drawableID = target.drawableID;
-            const dataURI = Scratch.Cast.toString(args.DATAURI);
+
+            if (!target) return;
     
             const image = new Image();
             image.onload = () => {
@@ -47,20 +53,16 @@
         
                 const skinId = Scratch.vm.renderer.createBitmapSkin(canvas);
                 Scratch.vm.renderer.updateDrawableSkinId(drawableID, skinId);
-                this.skins[drawableID] = skinId;
             };
-            image.src = dataURI;
+            image.src = Scratch.Cast.toString(DATAURI);
         }
 
-        resetSprite(args, util) {
+        removeSkin(_, util) {
             const target = util.target;
-            if (this.skins[target.drawableID]) {
-                target.setCostume(target.currentCostume);
-                delete this.skins[target.drawableID];
-            }
+            if (!target) return;
+            target.updateAllDrawableProperties();
         }
     }
   
-    Scratch.extensions.register(new BitmapSkinExtension());
+    Scratch.extensions.register(new QuickSkins());
 })(Scratch);
-  
