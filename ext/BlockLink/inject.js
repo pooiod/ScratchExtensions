52af1096a6f2a219fb588c9168be1db64b9efbee
pooiod7 <portsmouthuser35@gmail.com>
2025-03-21 17:31:57 -0400

    g

--- ext/BlockLink/inject.js
+++ ext/BlockLink/inject.js
@@ -63,7 +63,7 @@
 			updateWorkspace = () => {
 				var divider = document.querySelector('.divider_divider_1_Adi.menu-bar_divider_2VFCm');
 
-				var showIcon = document.querySelector("#app > div > div > div > div.gui_menu-bar-position_3U1T0.menu-bar_menu-bar_JcuHF.box_box_2jjDp > div.menu-bar_main-menu_3wjWH > div.menu-bar_file-group_1_CHX > div:nth-child(3) > img:nth-child(1)");
+				var showIcon = document.querySelector("#app > div > div > div > div.gui_menu-bar-position_3U1T0.menu-bar_menu-bar_JcuHF.box_box_2jjDp > div.menu-bar_main-menu_3wjWH > div.menu-bar_file-group_1_CHX > div:nth-child(4) > img:nth-child(1)");
 				if (showIcon) showIcon = getComputedStyle(showIcon).display != 'none';
 
 				if (showIcon) {
