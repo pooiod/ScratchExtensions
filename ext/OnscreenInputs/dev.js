f571f8000eb1d409d073415a13ceebd278e4e62f
dlovecchio2026 <contact.pooiod7@gmail.com>
2025-04-18 09:37:35 -0400

    Buttons

--- ext/OnscreenInputs/dev.js
+++ ext/OnscreenInputs/dev.js
@@ -7,6 +7,56 @@
 		throw new Error('This extension must run unsandboxed');
 	}
 
+    function setCSS(rawcss, id) {
+        var css = rawcss.replace(/ELEMENT/g, `.OnscreenInput.${id}`)
+            .replace(/size:/g, "width:")
+            .replace(/transform:/g, "transform: translate(-50%, -50%)");
+
+        if (document.getElementById(id) && document.getElementById(id).tagName === "STYLE" && document.getElementById(id).classList.contains("OnscreenInputStyle")) {
+            document.getElementById(id).textContent = css;
+            document.getElementById(id).setAttribute("raw", rawcss);
+            if (!css) {
+                document.getElementById(id).remove();
+            }
+        } else {
+            const style = document.createElement('style');
+            style.classList.add("OnscreenInputStyle");
+            style.setAttribute("raw", rawcss);
+            style.id = id;
+            style.textContent = css;
+            document.head.appendChild(style);
+        }
+    }
+
+    setCSS(`ELEMENT {
+    z-index: 9999;
+    touch-action: none;
+    position: absolute;
+    user-select: none;
+    transform:;
+}`, "OnscreenInput");
+
+    setCSS(`buttonELEMENT {
+    size: 20%;
+    aspect-ratio: 1;
+    border-radius: 50%;
+    border: 4px solid rgba(75, 75, 75, 0.2);
+    background-color: rgba(46, 46, 46, 0.6);
+    color: #fff;
+    cursor: pointer;
+    backdrop-filter: blur(10px);
+    transition: transform 0.1s ease, background-color 0.1s ease;
+}
+
+buttonELEMENT:hover {
+    transform: scale(0.97);
+}
+
+buttonELEMENT:active {
+    transform: scale(0.9);
+    background-color: rgba(29, 29, 29, 0.8);
+}`, "Default");
+
 	class P7OnscreenInputs {
 		constructor() {
             this.setVars = () => {
@@ -17,6 +67,7 @@
 
 			this.stageadd = (elm) => {
                 this.setVars();
+                this.canvas.parentElement.style.containerType = "size";
 				this.canvas.parentElement.appendChild(elm);
 			};
 
@@ -38,24 +89,68 @@
 			return {
 				id: 'P7OnscreenInputs',
 				name: 'Onscreen Inputs',
+                color1: "#4b4a60",
+                color2: "#383747",
 				blocks: [
+                    {
+                        opcode: 'addButton',
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: 'Add button [ID] with class [CLASS]',
+                        arguments: {
+                            ID: {
+                                type: Scratch.ArgumentType.STRING,
+                                defaultValue: "Button1"
+                            },
+                            CLASS: {
+                                type: Scratch.ArgumentType.STRING,
+                                defaultValue: "Default"
+                            }
+                        }
+                    },
+
+                    {
+                        opcode: 'removeButton',
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: 'Remove button [ID]',
+                        arguments: {
+                            ID: {
+                                type: Scratch.ArgumentType.STRING
+                            }
+                        }
+                    },
+
                     {
                         opcode: 'whenPressed',
                         blockType: Scratch.BlockType.HAT,
                         text: 'when [BUTTON] button pressed',
                         arguments: {
                             BUTTON: {
-                                type: Scratch.ArgumentType.STRING
+                                type: Scratch.ArgumentType.STRING,
+                                defaultValue: "Button1"
                             },
                         }
                     },
+
                     {
                         opcode: 'isButtonPressed',
                         blockType: Scratch.BlockType.BOOLEAN,
-                        text: '[BUTTON] button pressed?',
+                        text: 'button [BUTTON] pressed?',
                         arguments: {
                             BUTTON: {
-                                type: Scratch.ArgumentType.STRING
+                                type: Scratch.ArgumentType.STRING,
+                                defaultValue: "Button1"
+                            },
+                        }
+                    },
+
+                    {
+                        opcode: 'isButtonHovered',
+                        blockType: Scratch.BlockType.BOOLEAN,
+                        text: 'button [BUTTON] hovered?',
+                        arguments: {
+                            BUTTON: {
+                                type: Scratch.ArgumentType.STRING,
+                                defaultValue: "Button1"
                             },
                         }
                     },
@@ -85,7 +180,31 @@
                                 type: Scratch.ArgumentType.STRING
                             }
                         }
-                    }
+                    },
+
+                    {
+                        opcode: 'addClass',
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: 'Add class [CLASS] with css [CSS]',
+                        arguments: {
+                            CLASS: {
+                                type: Scratch.ArgumentType.STRING
+                            },
+                            CSS: {
+                                type: Scratch.ArgumentType.STRING
+                            }
+                        }
+                    },
+                    {
+                        opcode: 'removeClass',
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: 'Remove class [CLASS]',
+                        arguments: {
+                            CLASS: {
+                                type: Scratch.ArgumentType.STRING
+                            }
+                        }
+                    },
 				],
                 menus: {
                     visOptions: {
@@ -95,8 +214,19 @@
 			};
 		}
 
-		func(args) {
-			console.log(args);
+		addButton({ ID, CLASS }) {
+            this.setVars();
+
+            if (document.getElementById(ID)) {
+                return;
+            }
+
+            var button = document.createElement("BUTTON");
+            button.id = ID;
+            button.style.top = "50%";
+            button.style.left = "50%";
+            button.classList.add("OnscreenInput", CLASS || "Default");
+            this.stageadd(button);
 		}
 	}
 	Scratch.extensions.register(new P7OnscreenInputs());
