0f21686ce87de821e82ee838617f6000ecf3815d
dlovecchio2026 <contact.pooiod7@gmail.com>
2025-04-22 09:48:59 -0400

    Updated ScrollUtilities Added event hat for scroll Added zoom detection Added shift X scrolling Fixed canvas scrolling away

--- ext/ScrollUtilities/dev.js
+++ ext/ScrollUtilities/dev.js
@@ -1,70 +1,119 @@
 // Work in progress (scroll detector by pooiod7)
 
 (function (Scratch) {
-    'use strict';
-  
-    if (!Scratch.extensions.unsandboxed) {
-      throw new Error('This extension must run unsandboxed');
-    }
-  
-    class ScrollExtension {
-      constructor(runtime) {
-        this.isScrolling = false;
-        this.yvel = 0;
-        this.xvel = 0;
-  
-        document.addEventListener('wheel', this.handleScroll.bind(this), true);
-      }
-  
-      getInfo() {
-        return {
-          id: 'p7ScrollUtilities',
-          name: 'Scroll Utilities',
-          color1: '#4CAF50',
-          color2: '#45A164',
-          blocks: [
-            {
-              opcode: 'getScrollYVelocity',
-              blockType: Scratch.BlockType.REPORTER,
-              text: 'Scroll Y Velocity',
-            },
-            {
-              opcode: 'getScrollXVelocity',
-              blockType: Scratch.BlockType.REPORTER,
-              text: 'Scroll X Velocity',
-            },
-            {
-              opcode: 'scrolldetect',
-              blockType: Scratch.BlockType.BOOLEAN,
-              text: 'Scrolling?',
-            },
-          ],
-        };
-      }
-  
-      getScrollYVelocity() {
-        const vel = this.yvel;
-        this.yvel /= 5;
-        return vel;
-      }
-      
-      getScrollXVelocity() {
-        const vel = this.xvel;
-        this.xvel /= 5;
-        return vel;
-      }
-  
-      scrolldetect() {
-        return (Math.round(this.yvel) !== 0) || (Math.round(this.xvel) !== 0);
-      }
-  
-      handleScroll(scrolldata) {
-        this.yvel = parseInt(scrolldata.deltaY);
-        this.xvel = parseInt(scrolldata.deltaX);
-        this.isScrolling = true;
-      }
-    }
-  
-    Scratch.extensions.register(new ScrollExtension());
-  })(Scratch);
-  
+	'use strict';
+
+	if (!Scratch.extensions.unsandboxed) {
+		throw new Error('This extension must run unsandboxed');
+	}
+
+	class ScrollExtension {
+		constructor() {
+			this.isScrolling = false;
+			this.yvel = 0;
+			this.xvel = 0;
+			this.zvel = 0;
+			this.timeout = null;
+
+			document.addEventListener('wheel', this.handleScroll.bind(this), {
+				capture: true,
+				passive: false
+			});
+
+			// Scratch.vm.runtime.on('BEFORE_EXECUTE', () => {});
+		}
+
+		getInfo() {
+			return {
+				id: 'p7ScrollUtilities',
+				name: 'Scroll Utilities',
+				color1: '#4CAF50',
+				color2: '#45A164',
+				blocks: [
+					{
+						opcode: 'getScrollYVelocity',
+						blockType: Scratch.BlockType.REPORTER,
+						text: 'Scroll Y velocity',
+					},
+					{
+						opcode: 'getScrollXVelocity',
+						blockType: Scratch.BlockType.REPORTER,
+						text: 'Scroll X velocity',
+					},
+					{
+						opcode: 'getScrollZVelocity',
+						blockType: Scratch.BlockType.REPORTER,
+						text: 'Scroll Z velocity',
+					},
+
+					{
+						opcode: 'scrolldetect',
+						blockType: Scratch.BlockType.BOOLEAN,
+						text: 'Scrolling?',
+					},
+
+					{
+						blockType: Scratch.BlockType.EVENT,
+						opcode: 'onscroll',
+						text: 'On page scrolled',
+						isEdgeActivated: false
+					  }
+				],
+			};
+		}
+
+		getScrollYVelocity() {
+			return this.yvel;
+		}
+		
+		getScrollXVelocity() {
+			return this.xvel;
+		}
+
+		getScrollZVelocity() {
+			return this.zvel;
+		}
+
+		scrolldetect() {
+			return this.isScrolling;
+		}
+
+		handleScroll(scrolldata) {
+			if (scrolldata.target && scrolldata.target.tagName === 'CANVAS') {
+				scrolldata.preventDefault();
+			}
+
+			this.xvel = parseInt(scrolldata.deltaX);
+			this.isScrolling = true;
+
+			if (scrolldata.ctrlKey) {
+				this.yvel = 0;
+				this.zvel = parseInt(scrolldata.deltaY);
+			} else {
+				this.zvel = parseInt(scrolldata.deltaZ);
+				this.yvel = parseInt(scrolldata.deltaY);
+			}
+
+			if (scrolldata.shiftKey) {
+				var tmpy = this.yvel;
+				this.yvel = this.xvel;
+				this.xvel = tmpy;
+			}
+
+			Scratch.vm.runtime.startHats('p7ScrollUtilities_onscroll');
+
+			clearTimeout(this.timeout);
+			this.timeout = setTimeout(() => {
+				this.timeout = null;
+				if (this.isScrolling) {
+					this.isScrolling = false;
+					this.yvel = 0;
+					this.xvel = 0;
+					this.zvel = 0;
+				}
+			}, 50);
+		}
+	}
+
+	Scratch.extensions.register(new ScrollExtension());
+})(Scratch);
