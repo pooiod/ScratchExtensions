40c409d5a23833c3c936a57b6f88266888d595a5
pooiod7 <portsmouthuser35@gmail.com>
2025-05-27 12:58:10 -0400

    f

--- ext/PageOpener/main.js
+++ ext/PageOpener/main.js
@@ -16,6 +16,17 @@
                 color2: "#7f4ee0",
                 blocks: [
                     {
+                        opcode: 'redirect',
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: 'Open [URL] in the current tab',
+                        arguments: {
+                            URL: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'https://example.com',
+                            }
+                        },
+                    },
+                    {
                         opcode: 'openTab',
                         blockType: Scratch.BlockType.COMMAND,
                         text: 'Open [URL] in new tab',
@@ -47,6 +58,10 @@
 
         openTab({ URL }) {  
             window.open(URL, '_blank');
+        }
+
+        redirect({ URL }) {  
+            window.location.href = URL;
         }
 
         openWindow({ URL, SETTINGS }) {  
